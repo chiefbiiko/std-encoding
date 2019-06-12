7a7278c16698d1173a6b75199aa3bd537cf50396
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-06-12 20:56:38 +0200

    transcoding

---
+++ README.md
@@ -0,0 +1,25 @@
+# std-encoding
+
+[![Travis](http://img.shields.io/travis/chiefbiiko/std-encoding.svg?style=flat)](http://travis-ci.org/chiefbiiko/std-encoding) [![AppVeyor](https://ci.appveyor.com/api/projects/status/github/chiefbiiko/std-encoding?branch=master&svg=true)](https://ci.appveyor.com/project/chiefbiiko/std-encoding)
+
+## Usage
+
+``` ts
+import { encode, decode } from "https://denopkg.com/chiefbiiko/std-encoding/mod.ts";
+
+console.log("ACAB in hex", decode(encode("ACAB", "utf8"), "hex"));
+```
+
+## API
+
+#### `encode(str: string, encoding: string = "utf8"): Uint8Array`
+
+Encodes a string assuming the specified encoding format.
+
+#### `decode(buf: Uint8Array, encoding: string = "utf8"): string`
+
+Decodes a buffer to a string with the indicated encoding format.
+
+## License
+
+[MIT](./LICENSE)
---
+++ test.ts
@@ -0,0 +1,38 @@
+import { test, runIfMain } from "https://deno.land/x/testing/mod.ts";
+import { assertEquals } from "https://deno.land/x/testing/asserts.ts";
+import { encode, decode } from "./mod.ts";
+
+test({
+  name: "base64 encoding and decoding",
+  fn(): void {
+    const msg: string = "QUNBQg==";
+    const buf: Uint8Array = encode(msg, "base64");
+    const str: string = decode(buf, "base64");
+    assertEquals(buf, Uint8Array.from([0x41, 0x43, 0x41, 0x42]))
+    assertEquals(str, msg)
+  }
+})
+
+test({
+  name: "hex encoding and decoding",
+  fn(): void {
+    const msg: string = "41434142";
+    const buf: Uint8Array = encode(msg, "hex");
+    const str: string = decode(buf, "hex");
+    assertEquals(buf, Uint8Array.from([0x41, 0x43, 0x41, 0x42]))
+    assertEquals(str, msg)
+  }
+})
+
+test({
+  name: "utf8 encoding and decoding",
+  fn(): void {
+    const msg: string = "ACAB";
+    const buf: Uint8Array = encode(msg, "utf8");
+    const str: string = decode(buf, "utf8");
+    assertEquals(buf, Uint8Array.from([0x41, 0x43, 0x41, 0x42]))
+    assertEquals(str, msg)
+  }
+})
+
+runIfMain(import.meta, { parallel: true })
