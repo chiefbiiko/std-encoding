69c44773add5e37d1f2f2b94e810984e5a58256a
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-06-28 12:58:29 +0200

    test: url safe case

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assert, assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { encode, decode } from "./mod.ts";
 
 Deno.test({
@@ -7,7 +7,9 @@ Deno.test({
     const msg: string = "QUNBQg==";
     const buf: Uint8Array = encode(msg, "base64");
     const str: string = decode(buf, "base64");
+
     assertEquals(buf, Uint8Array.from([0x41, 0x43, 0x41, 0x42]));
+
     assertEquals(str, msg);
   },
 });
@@ -18,7 +20,9 @@ Deno.test({
     const msg: string = "41434142";
     const buf: Uint8Array = encode(msg, "hex");
     const str: string = decode(buf, "hex");
+
     assertEquals(buf, Uint8Array.from([0x41, 0x43, 0x41, 0x42]));
+
     assertEquals(str, msg);
   },
 });
@@ -29,7 +33,25 @@ Deno.test({
     const msg: string = "ACAB";
     const buf: Uint8Array = encode(msg, "utf8");
     const str: string = decode(buf, "utf8");
+
     assertEquals(buf, Uint8Array.from([0x41, 0x43, 0x41, 0x42]));
+
+    assertEquals(str, msg);
+  },
+});
+
+Deno.test({
+  name: "opt-in base64 url safeness",
+  fn(): void {
+    const msg: string = "function cb() {}";
+    const buf: Uint8Array = encode(msg, "base64url");
+    const str: string = decode(buf, "base64url");
+
+    assert(!str.includes("-"));
+    assert(str.includes("+"));
+    assert(!str.includes("_"));
+    assert(!str.includes("."));
+
     assertEquals(str, msg);
   },
 });
